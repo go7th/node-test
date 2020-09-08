4ff2a34a3af52063106c05a282baebcb1b13f1bb
Xing Wang (PSP) <xwang@xwang18676.starbucks.net>
2020-09-08 17:31:30 +0800

    [node-test] add /menu/detail

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,29 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: '链接列表' });
 });
+router.post('/menu/detail', function(req, res, next) {
+  console.log(req.body);
+  console.log(req.query);
+  const data  = {
+    "category": {
+        "name": "Danielle_McLaughlin",
+        "products": [
+            {
+                "id": "2",
+                "name": "Camilla62",
+                "code": "33ab4367-20d2-4997-a5e3-3a5a6ca2e98a",
+                "qty": 91891,
+                "image": "http://billie.name"
+            }
+        ]
+    }
+}
+  res.send({
+    code: 100,
+    data,
+    message: "success"
+});
+
+});
 
 module.exports = router;
